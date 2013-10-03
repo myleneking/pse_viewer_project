a5f6b1f071b8adecc7c783df0f9b1c76cb77dde2
Mylene King <mylene.king@gmail.com>
2013-10-03 19:17:30 +0800

    master: minor fix.

--- src/com/p130001/pseviewer/adapter/StockAdapter.java
+++ src/com/p130001/pseviewer/adapter/StockAdapter.java
@@ -11,8 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class StockAdapter extends BaseAdapter{
